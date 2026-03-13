c47d9480d286970ef8e62507e7c357b9a658cfd6
revelation4825-tech <revelation4.8.25@gmail.com>
2026-03-13 12:29:20 -0600

    Basic form handling and added Typewriter plugin

--- app.js
+++ app.js
@@ -2,14 +2,32 @@ let headElement = document.querySelector("#head")
 let head = ("Welcome To AI")
 headElement.innerHTML = head
 
-let button = document.querySelector("#send-button")
+function generateAnswer(response) {
 
-function showAnswer(response) {
-    console.log(response.data.answer)
+let answerElement = document.querySelector("#answer-output")
+answerElement.innerHTML = (response.data.answer)
+
 }
+function generateOutput(event) {
+    event.preventDefault();
+
+new Typewriter('#answer-output', {
+  strings: ['Silent whisper, Moonlit dreams, Time drifts softly, Life gentle seams.'],
+  autoStart: true,
+  delay: 1,
+  cursor: "",
+});
+
+}
+
+let formElement = document.querySelector("#ask-form")
+formElement.addEventListener("submit", generateOutput)
+
 let apiKey = "203adfb889341eafafcd3o792bteb01b";
-let prompt = "Who was the first female president";
-let context = "be precise and provide short response without explaining much about your reasoning behind your opinion";
+let askElement = document.querySelector("#ask-input");
+let prompt = (askElement.value);
+//let context = `provide shortest poems based on ${prompt} without explaining much about your reasoning behind your opinion`;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-axios.get(apiUrl).then(showAnswer)
+
+axios.get(apiUrl).then(generateAnswer)
