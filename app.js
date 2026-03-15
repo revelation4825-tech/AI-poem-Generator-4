695a677cabebafc092e6a979a7e29f852a960daf
revelation4825-tech <revelation4.8.25@gmail.com>
2026-03-14 19:38:54 -0600

    Improved the User Experience

--- app.js
+++ app.js
@@ -2,32 +2,31 @@ let headElement = document.querySelector("#head")
 let head = ("Welcome To AI")
 headElement.innerHTML = head
 
-function generateAnswer(response) {
-
-let answerElement = document.querySelector("#answer-output")
-answerElement.innerHTML = (response.data.answer)
-
-}
-function generateOutput(event) {
-    event.preventDefault();
-
+function displayAnswer(response) {
+    
 new Typewriter('#answer-output', {
-  strings: ['Silent whisper, Moonlit dreams, Time drifts softly, Life gentle seams.'],
+  strings: response.data.answer,
   autoStart: true,
   delay: 1,
   cursor: "",
 });
 
 }
-
-let formElement = document.querySelector("#ask-form")
-formElement.addEventListener("submit", generateOutput)
+function generateOutput(event) {
+    event.preventDefault();
 
 let apiKey = "203adfb889341eafafcd3o792bteb01b";
 let askElement = document.querySelector("#ask-input");
 let prompt = (askElement.value);
-//let context = `provide shortest poems based on ${prompt} without explaining much about your reasoning behind your opinion`;
+let context = `generate shortest Korean poems each time based on ${prompt} only in a 5 line and do not forget to seperate each line with a <br/>. sign the poem at the end with '다나 AI' inside a <strong> element.`;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+let answerElement = document.querySelector("#answer-output");
+answerElement.classList.remove("hidden");
+answerElement.innerHTML = `<div class="blink">⏳ Generating a Korean poem about ${prompt}</div>`
 
-axios.get(apiUrl).then(generateAnswer)
+axios.get(apiUrl).then(displayAnswer);
+}
+
+let formElement = document.querySelector("#ask-form");
+formElement.addEventListener("submit", generateOutput);
